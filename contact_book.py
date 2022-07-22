abc44b7ecad6eb127f74d1dc4ab20af751dec280
JustLittleYeti <50328199+JustLittleYeti@users.noreply.github.com>
2022-07-22 12:51:17 +0200

    Update contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -7,18 +7,21 @@ from termcolor import colored
 
 #FUNCTIONS
 def create_new(book):
+    """This function creates new contact book. It takes one positional argument [name of the book]"""
     with open(book,"w",newline="") as csvfile:
         fieldnames=["Last Name", "First name", "Phone number", "Mail", "Address"]
         writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
         writer.writeheader()
 
 def create_entry(book):
-        with open(book,"a",newline="") as csvfile:
-            fieldnames=["Last Name", "First name", "Phone number", "Mail", "Address"]
-            writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
-            writer.writerow({"Last Name":input("Please, provide last name for the contact: "), "First name":input("Please, provide first name for the contact: "), "Phone number":input("Please, provide phone number for the contact: "), "Mail":input("Please, provide email address for the contact: "), "Address":input("Please, provide address for the contact: ")})
+    """This function creates new entry in given contact book. It takes one positional argument [name of the book]"""
+    with open(book,"a",newline="") as csvfile:
+        fieldnames=["Last Name", "First name", "Phone number", "Mail", "Address"]
+        writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+        writer.writerow({"Last Name":input("Please, provide last name for the contact: "), "First name":input("Please, provide first name for the contact: "), "Phone number":input("Please, provide phone number for the contact: "), "Mail":input("Please, provide email address for the contact: "), "Address":input("Please, provide address for the contact: ")})
 
 def look_for(book,contact):
+    """This function look for given info in given contact book. It takes two positional argument [name of the book] and [info]"""
     with open(book,"r",newline="") as csvfile:
         fieldnames=["Last Name", "First name", "Phone number", "Mail", "Address"]
         reader = csv.DictReader(csvfile,fieldnames=fieldnames)
@@ -31,6 +34,7 @@ def look_for(book,contact):
                 print(colored("Last Name:","yellow"), row['Last Name'],colored(" First name:","yellow"), row['First name'],colored(" Phone number:","yellow"), row['Phone number'],colored(" Mail:","yellow"), row['Mail'],colored(" Address:","yellow"), row['Address'])
 
 def show_book(book):
+    """This function shows contact book. It takes one positional argument [name of the book]"""
     with open(book,"r",newline="") as csvfile:
         fieldnames=["Last Name", "First name", "Phone number", "Mail", "Address"]
         reader = csv.DictReader(csvfile,fieldnames=fieldnames)
